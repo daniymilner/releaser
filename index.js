b4046b04a2fe87fb36233ce0a7ffdfe65b319e68
Aleksey Olenyuk <alo@qapint.com>
2015-10-03 21:32:02 +0300

    add utils for reading files

--- index.js
+++ index.js
@@ -2,20 +2,21 @@ var semver = require('semver'),
 	exec = require('child-process-promise').exec,
 	cwd = process.cwd(),
 	fs = require('fs'),
-	indent = require('detect-indent');
+	indent = require('detect-indent'),
+	utils = require('./utils');
 
 exports.manifests = function(){
 	return ['package.json', 'bower.json'].filter(function(manifest){
-		return fs.existsSync(cwd + '/' + manifest);
+		return utils.exists(cwd + '/' + manifest);
 	});
 };
 
 exports.bump = function(manifest, type){
 	var pkg = cwd + '/' + manifest,
-		current = require(pkg),
-		usedIndent, version;
+		current = utils.readJSON(pkg),
+		usedIndent;
 	current.version = semver.inc(current.version, type);
-	version = current.version;
-	usedIndent = indent(fs.readFileSync(pkg, 'utf8')) || '  ';
-	fs.writeFileSync(pkg, JSON.stringify(current, null, usedIndent));
+	usedIndent = indent(utils.readFileSync(pkg)) || '  ';
+	console.log(usedIndent);
+	utils.writeJSON(pkg, current, usedIndent);
 };
---
+++ utils.js
@@ -0,0 +1,28 @@
+var fs = require('fs-extra');
+
+exports.readJSON = function(pathSrc){
+	var data = {}, json;
+	if(exports.exists(pathSrc)){
+		json = exports.readFileSync(pathSrc);
+		try{
+			data = JSON.parse(json);
+		}catch(e){
+			console.log(e);
+			data = {};
+		}
+	}
+	return data;
+};
+
+exports.writeJSON = function(pathDest, data, indent){
+	fs.writeFileSync(pathDest, JSON.stringify(data, null, indent));
+};
+
+exports.exists = function(){
+	return [].every.call(arguments, fs.existsSync);
+};
+
+exports.readFileSync = function(pathSrc){
+	return fs.readFileSync(pathSrc, 'utf8');
+};
+
