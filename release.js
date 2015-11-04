a097e9505d768aa79a2dced697184fa97943ed9c
Aleksey Olenyuk <alo@qapint.com>
2015-11-04 22:11:58 +0200

    add parameter 'full'

--- release.js
+++ release.js
@@ -4,6 +4,9 @@ var program = require('commander'),
 	minimist = require('minimist')(process.argv.slice(2)),
 	api = require('./utils/api'),
 	lock = false,
+	logger = require('tracer').colorConsole({
+		format: "{{message}}"
+	}),
 	branch;
 
 program
@@ -12,7 +15,8 @@ program
 	.option('--branch [name]', '[branch] - set local branch name')
 	.option('--tag', '[option] - create tag with new version')
 	.option('--push', '[option] - push changes to current branch')
-	.option('--master', '[option] - merge changes to master');
+	.option('--master', '[option] - merge changes to master')
+	.option('--full', '[option] - "--tag --push --master"');
 
 ['major', 'minor', 'patch'].forEach(function(type){
 	program.option('--' + type, '[type] - increase ' + type + ' version');
@@ -28,7 +32,15 @@ program
 				if(program.branch && typeof minimist['branch'] === 'string' && minimist['branch']){
 					branch = minimist['branch'];
 					api.checkout(branch).then(function(){
-						if(program.tag){
+						if(program.full){
+							api.tag()
+								.then(function(){
+									return api.push(branch);
+								})
+								.then(function(){
+									api.merge(branch);
+								})
+						}else if(program.tag){
 							api.tag().then(function(){
 								if(program.push){
 									api.push(branch).then(function(){
@@ -40,6 +52,8 @@ program
 							})
 						}
 					});
+				}else{
+					logger.error('expect parameter --branch [name]')
 				}
 			}, 0);
 		}
